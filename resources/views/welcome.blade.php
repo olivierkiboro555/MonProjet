7054cb910430d65b31725f551d82f53c62e24294
olivierkiboro555 <olivierkiboro5@gmail.com>
2024-01-14 20:14:41 +0100

    Premier commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="Fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GameCineStore : home</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="container">
+
+        <h1 class="text-center hover:text-lef hover:bg-orange-400">
+             thinkstrong : Dev
+        </h1>
+      
+    </div>
+</body>
+</html>
+
