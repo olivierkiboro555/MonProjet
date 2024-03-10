15893a0aca76985d35fe95f3f752fd303484552d
olivierkiboro555 <olivierkiboro5@gmail.com>
2024-03-10 18:57:33 +0100

    10/03/2024 00h23 fin ep 10

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,6 +10,6 @@ class PostController extends Controller
     public function index()
 
     {
-        return 'hello world';
+        return view('Posts.index');
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: [
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
-    "./resources/**/*.vue",
   ],
+
   theme: {
-    extend: {},
+
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+
   },
-  plugins: [],
+
+  plugins: [
+
+    require('@tailwindcss/forms')({
+      strategy: 'class',
+    }),
+
+  ],
 }
